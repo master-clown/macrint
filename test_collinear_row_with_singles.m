60374a31a548bc1c2928e8acb3f69159eb157365
Abdulla Abakarov <abakarov97@gmail.com>
2023-03-18 20:06:08 +0600

    Finished "single crack vs collinear row" STs comparison
    Looks like the Kachanov method cannot take into account the "virtual" cracks.
    At least at the stage of matrix assembly. Even a single crack case (for
    collinear row ST) must yield the correct SIFs (1.4142), but we have 1.25 -
    this is the SIF for a single crack whatsoever
    
    The method requires a modification, which will take into account:
    - the virtual cracks which ACCOMPANY the physical one
    - the influence of the virtual cracks from one row on the physical crack from the other
    - the influence of the virtual cracks from one row on the virtual cracks from the other
    
    The last aspect may be unnecessary since the virtual cracks are identical to the physical.
    
    I repeat: at least the mean tractions must be evaluated with those aspects taken into
    account. The SIF evaluation formula must be reconsidered either.

--- test_collinear_row_with_singles.m
+++ test_collinear_row_with_singles.m
@@ -1,6 +1,6 @@
 periods = [ 2 ];
 hls = 0.5;
-numCracksVert = 2;
+numCracksVert = 1;
 vertSpacing = 1;
 bstLst = zeros(2, 2, 3);
     bstLst(2, 2, 1) = 1;
@@ -23,8 +23,11 @@ for iPeriod = 1:numel(periods)
     period = periods(iPeriod);
     fnDetails = sprintf("period=%0.3f", period);
 
-    [sifColSol, avgTrColSol] = evaluateUsingCollinearSt(periods(1), hls(1), numCracksVert, vertSpacing, bstLst);
-    save(strcat(resultsDir, "collinear-row-st-sol_", fnDetails, ".mat"), "sifColSol", "avgTrColSol");
+    [sifMatColSol, avgTrVecColSol] = evaluateUsingCollinearSt(periods(1), hls(1), numCracksVert, vertSpacing, bstLst);
+    save(strcat(resultsDir, "collinear-row-st-sol_", fnDetails, ".mat"), "sifMatColSol", "avgTrVecColSol");
+
+    [sifMatSinSol, avgTrVecSinSol] = evaluateUsingSingleCrackSt(periods(1), hls(1), numCracksVert, vertSpacing, bstLst);
+    save(strcat(resultsDir, "single-crack-st-sol_", fnDetails, ".mat"), "sifMatSinSol", "avgTrVecSinSol");
 end
 
 
@@ -46,15 +49,19 @@ function [sifMat, avgTract] = evaluateUsingCollinearSt(period, hl, numCracksVert
     if false
         figure;    
         plot(ccLst(:, 1), ccLst(:, 2), 'o');
-    end
 
-    [sifMat, avgTract] = crack_interact(ccLst, cpLst, chlLst, bstLst, sifCrackEvalLst, st_func);
+        sifMat = [];
+        avgTract = [];
+    else
+        [sifMat, avgTract] = crack_interact(ccLst, cpLst, chlLst, bstLst, sifCrackEvalLst, st_func);
+    end
 end
 
 function [sifMat, avgTract] = evaluateUsingSingleCrackSt(period, hl, numCracksVert, vertSpacing, bstLst)
 
-    numHorCracksOneSide = 5; % how many horizontal cracks are added for each crack in the stack from ONE SIDE (i.e. the total number is double)
-    numCracks = (2*numHorCracksOneSide + 1)*numCracksVert;
+    numHorCracksOneSide = 40; % how many horizontal cracks are added for each crack in the stack from ONE SIDE (i.e. the total number is double plus one)
+    numHorCracksOneLine = (2*numHorCracksOneSide + 1);
+    numCracks = numHorCracksOneLine*numCracksVert;
 
     ccLst = zeros(numCracks, 2);
     cpLst = zeros(numCracks, 1);
@@ -62,16 +69,71 @@ function [sifMat, avgTract] = evaluateUsingSingleCrackSt(period, hl, numCracksVe
     sifCrackEvalLst = 1:numCracksVert;
     st_func = @st_single_crack;
 
-    % CONTINUE HERE! Stopped at 13:00, 16/03/23
+    for iHor = 1:numHorCracksOneLine
+        if iHor > 1
+            xCenter = xCenter - (-1)^iHor * (iHor - 1) * period;
+        else
+            xCenter = 0;
+        end
 
-    for i = 2:numCracksVert
-        ccLst(i, 2) = ccLst(i - 1, 2) - (-1)^i * (i - 1) * vertSpacing;
+        for iVer = 1:numCracksVert
+            crIdx = (iHor - 1)*numCracksVert + iVer;
+            ccLst(crIdx, 1) = xCenter;
+
+            if iVer > 1
+                ccLst(crIdx, 2) = ccLst(crIdx - 1, 2) - (-1)^iVer * (iVer - 1) * vertSpacing;
+            end
+        end
     end
 
     if false
         figure;    
         plot(ccLst(:, 1), ccLst(:, 2), 'o');
-    end
 
-    [sifMat, avgTract] = crack_interact(ccLst, cpLst, chlLst, bstLst, sifCrackEvalLst, st_func);
+        sifMat = [];
+        avgTract = [];
+    else
+        [sifMat, avgTract] = crack_interact(ccLst, cpLst, chlLst, bstLst, sifCrackEvalLst, st_func);
+    end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
